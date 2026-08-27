e594b14a9d33b324f0b8c24db949536c20808265
rohuvs <joysunny0709@gmail.com>
2026-08-27 17:24:07 +0900

    Add event listeners for button clicks

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const startButton = document.getElementById("start-button");
+const labButton = document.getElementById("lab-button");
+const aboutButton = document.getElementById("about-button");
 
+startButton.addEventListener("click", function () {
+    alert("MISSION MODE will be available soon.");
+});
+
+labButton.addEventListener("click", function () {
+    alert("FREE EXPERIMENT will be available soon.");
+});
+
+aboutButton.addEventListener("click", function () {
+    alert("MEMBRANE\nA Virtual Cell Membrane Laboratory");
+});
